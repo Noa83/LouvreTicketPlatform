98dea007a69932e47862623876dbef1c08ce3002
Noa83 <audrey.bc83@gmail.com>
2017-01-18 21:28:45 +0100

    Création du FormModel

--- src/Louvre/TicketPlatformBundle/Model/FormModel.php
+++ src/Louvre/TicketPlatformBundle/Model/FormModel.php
@@ -3,6 +3,7 @@
 namespace Louvre\TicketPlatformBundle\Model;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * FormModel
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\MappedSuperClass
  */
 
-abstract class FormModel
+class FormModel
 {
     /**
      * @var \DateTime
@@ -74,5 +75,232 @@ abstract class FormModel
      * @ORM\Column(name="reducedPrice", type="boolean")
      */
     private $reducedPrice;
+
+
+    public function __construct()
+    {
+        $this->birthDate = new \DateTime();
+        $this->visitDate = new \DateTime();
+
+    }
+
+
+    /**
+     * Set visitDate
+     *
+    @param \DateTime $visitDate
+     *
+     * @return FormModel
+     */
+    public function setVisitDate($visitDate)
+    {
+        $this->visitDate = $visitDate;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDate
+     *
+     * @return \DateTime
+     */
+    public function getVisitDate()
+    {
+        return $this->visitDate;
+    }
+
+    /**
+     * Set ticketType
+     *
+     * @param string $ticketType
+     *
+     * @return FormModel
+     */
+    public function setTicketType($ticketType)
+    {
+        $this->ticketType = $ticketType;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketType
+     *
+     * @return string
+     */
+    public function getTicketType()
+    {
+        return $this->ticketType;
+    }
+
+    /**
+     * Set numberOfTickets
+     *
+     * @param integer $numberOfTickets
+     *
+     * @return FormModel
+     */
+    public function setNumberOfTickets($numberOfTickets)
+    {
+        $this->numberOfTickets = $numberOfTickets;
+
+        return $this;
+    }
+
+    /**
+     * Get numberOfTickets
+     *
+     * @return int
+     */
+    public function getNumberOfTickets()
+    {
+        return $this->numberOfTickets;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return FormModel
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return FormModel
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return FormModel
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set birthDate
+     *
+    @param \DateTime $birthDate
+     *
+     * @return FormModel
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return FormModel
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set reducedPrice
+     *
+     * @param boolean $reducedPrice
+     *
+     * @return FormModel
+     */
+    public function setReducedPrice($reducedPrice)
+    {
+        $this->reducedPrice = $reducedPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get reducedPrice
+     *
+     * @return bool
+     */
+    public function getReducedPrice()
+    {
+        return $this->reducedPrice;
+    }
+
+
 
 }
