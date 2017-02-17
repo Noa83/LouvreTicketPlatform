e01d769ad1a6663fb9e6d8a320fc31332cae73a3
Noa83 <audrey.bc83@gmail.com>
2017-02-17 15:22:22 +0100

    Creation du service StripePay de paiement

---
+++ src/Louvre/TicketPlatformBundle/Services/StripePay.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Louvre\TicketPlatformBundle\Services;
+
+use Stripe\Stripe;
+use Stripe\Charge;
+
+class StripePay extends Stripe
+{
+    public function __construct()
+    {
+        self::setApiKey('sk_test_Lv5I6Urhq0UOhJwDurhD1cna');
+
+        return $this;
+    }
+
+    /**
+     * @param int $chargeAmount: The charge amount in cents
+     * @param string $chargeCurrency: The charge currency to use
+     * @param string $token: The payment token returned by the payment form (Stripe.js)
+     *
+     * @return Charge
+     */
+    public function createCharge($chargeAmount, $chargeCurrency, $token)
+    {
+
+        $chargeOptions = [
+            'amount'        => $chargeAmount,
+            'currency'      => $chargeCurrency,
+            'source'        => $token,
+        ];
+
+        return Charge::create($chargeOptions);
+    }
+
+}
